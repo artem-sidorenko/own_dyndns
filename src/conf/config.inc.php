74e9da20f7db5b80d891d9dba58f86dbc77a46dd
Artem Sidorenko <artem@2realities.com>
2013-12-24 14:03:12 +0100

    changed the password hashing

--- src/conf/config.inc.php
+++ src/conf/config.inc.php
@@ -5,7 +5,7 @@ $dns_ttl=60;
 $dns_server="localhost";
 
 $users=[ 
-	"artem" => ["password"=> "lala", "zones"=> ["vpn.scader.eu"] ],
+	"test" => ["password"=> "$6$ghshakldwqddr$1K3nY6KTZ6CU2a63YSI7ZQ2lkLhOTB407YkJBEFjajvhtGrq14QavlUTzUWjlzv9VpnLbzMx1h3e2d/gxjv17.", "zones"=> ["test.scader.eu"] ],
 	];
 
 ?>
--- src/index.php
+++ src/index.php
@@ -1,5 +1,6 @@
 <?php
 # Custom DDNS updates without any dyndns providers on the own infrastructure
+# PHP 5.5 required due to password_verify() function
 # Licensed under BSD License: http://www.opensource.org/licenses/BSD-2-Clause
 # Author: Artem Sidorenko http://2realities.com
 
@@ -14,12 +15,14 @@ $domain=$_GET["domain"];
 $password=$_GET["pass"];
 
 #check auth
-if( (!isset($users[$username])) || ($password!=$users[$username]["password"]) || (!in_array($users[$username]["zones"])) ){
+if( (!isset($users[$username])) || password_verify($password,$users[$username]["password"]) || (!in_array($users[$username]["zones"])) ){
 	die("authorisation failed");
 }else{
 	#auth if ok, proceed with update
 	$zone=substr($domain,strpos($domain,"."));
-	$commands="server $dns_server\\nzone $zone\\nupdate delete $domain\\nupdate add $domain $dns_ttl A $ip\\nsend\\n";
+	$commands="server $dns_server\\nzone $zone\\nupdate delete $domain\\nupdate add $domain $dns_ttl A $ip\\n";
+	#ready to run the update
+	$commands.="send\\n";
 	$output=array(); $returnvar=0;
 	exec('echo -e "'.$commands.'" | nsupdate -k '.$dns_key_file,$output,$returnvar);
 	if($returnvar==0)
